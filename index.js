dce7b33f90614974436a8f533c08e6d7a75e285b
morteza <morteza.memarian81@gmail.com>
2023-09-15 08:52:29 +0330

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+
+///aler
+
+
+   
+//let name=prompt();
+
+
+
+/*if(name="jerard"
+){
+   alert("ok")
+;}
+
+   else{alert("bad")}
+}*/
+/*switch(name){
+   case "gerrard":alert("baaaaad")
+    break;
+    case "jerrard":alert("goood")
+    break;
+    default:
+      alert("It must start with the letter j");
+}*/
+/*switch(prompt()){
+   case indexof[0] == "g":alert("baaaaad")
+    break;
+    case indexof[0] == "j":alert("goood")
+    break;
+    default:*/
+      alert("It must start with the letter j");}
+      let findpassingstudent = [25,45,98,22,42,36,88,75,58];
+      for(leti=0;i>findpassingstudent.length;i++){
+         console.log(i)
+      }
+      
+
+
---
+++ mortezamemarian-HW1-maktab-119/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+   
+/*let name=prompt();
+
+
+
+
+switch(name){
+   case "gerrard":alert("baaaaad")
+    break;
+    case "jerrard":alert("goood")
+    break;
+    default:
+      alert("It must start with the letter j");
+}*/
+let num =[14,54,96,85,76,32,65,47];
+
+ for(i=0;i >= 60;i++){
+  console.log(num[i])
+ }
